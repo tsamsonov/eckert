f266e5f034f8267468b1c8f0eea5c8019ba8bd75
Timofey Samsonov <tsamsonov@geogr.msu.ru>
2024-04-06 17:35:40 +0300

    Start voronoy tree function

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,40 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+extern crate geo;
+extern crate voronoice;
+use geo::{Coord, Point, LineString, Polygon};
+use voronoice::{Point as VoronoiPoint, VoronoiBuilder};
+
+fn voronoy_tree(points: &Vec<Point>) -> Vec<Polygon> {
+    // let orders = vec![0.; points.len()];
+
+    let sites = points
+        .iter()
+        .map(|&p| VoronoiPoint { x: p.x(), y: p.y() })
+        .collect();
+
+    let diagram = VoronoiBuilder::default()
+        .set_sites(sites)
+        .set_lloyd_relaxation_iterations(5)
+        .build()
+        .unwrap();
+
+    let cells = diagram
+        .iter_cells()
+        .map(|cell|
+             Polygon::new(
+                 LineString::new(
+                    cell
+                        .iter_vertices()
+                        .collect::<Vec<&VoronoiPoint>>()
+                            .iter()
+                            .map(|&vp| Coord {x: vp.x, y: vp.y})
+                            .collect()
+                 ),
+                 vec![]
+             )
+        )
+        .collect();
+
+
+    return cells;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
